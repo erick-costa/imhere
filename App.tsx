109a9ece5c8198535eca598d66108ae729f17873
erick-costa <erickcosta2509@gmail.com>
2024-11-05 15:11:17 -0300

    organizing project

--- App.tsx
+++ App.tsx
@@ -1,28 +1,5 @@
-import { Text, View, StyleSheet } from "react-native"
+import { Home } from "./src/screens/Home"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022.</Text>
-    </View>
-  )
+  return <Home />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#131016",
-    padding: 24,
-  },
-  eventName: {
-    color: "#fdfcfe",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 48,
-  },
-  eventDate: {
-    color: "#6b6b6b",
-    fontSize: 16,
-  },
-})
