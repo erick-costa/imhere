658647447832a13ea7719cc9a72b348820753bec
erick-costa <erickcosta2509@gmail.com>
2024-11-05 15:27:16 -0300

    text input

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,13 +7,21 @@ export const styles = StyleSheet.create({
     padding: 24,
   },
   eventName: {
-    color: "#fdfcfe",
+    color: "#FFF",
     fontSize: 24,
     fontWeight: "bold",
     marginTop: 48,
   },
   eventDate: {
     color: "#6b6b6b",
+    fontSize: 16,
+  },
+  input: {
+    height: 56,
+    backgroundColor: "#1f1e25",
+    borderRadius: 5,
+    color: "#FFF",
+    padding: 16,
     fontSize: 16,
   },
 })
